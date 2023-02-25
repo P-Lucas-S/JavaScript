79d6a5d86b2e6dbf55a344fe48aff6214d02edae
P-Lucas-S <68026747+P-Lucas-S@users.noreply.github.com>
2023-02-25 14:46:56 -0300

    ex015.1

--- aula12ex/ex015.1/script.js
+++ aula12ex/ex015.1/script.js
@@ -1,1 +1,9 @@
-alert('Olá!')
+function idade(){
+    var agora = new Date()
+    var ano = agora.getFullYear()
+    var nasc = window.document.getElementById('inasc')
+    var idade = Number(ano) - Number(nasc.value)
+    console.log(idade)
+    var sex = window.document.getElementsByName('sex')
+    console.log(sex)
+}
