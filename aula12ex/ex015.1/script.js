d3aa4e58482eac9578431baf65dc10f3a0490dd4
P-Lucas-S <68026747+P-Lucas-S@users.noreply.github.com>
2023-02-26 23:01:33 -0300

    ex015

--- aula12ex/ex015.1/script.js
+++ aula12ex/ex015.1/script.js
@@ -1,9 +1,17 @@
 function idade(){
-    var agora = new Date()
-    var ano = agora.getFullYear()
+    var ano = new Date().getFullYear()
     var nasc = window.document.getElementById('inasc')
-    var idade = Number(ano) - Number(nasc.value)
-    console.log(idade)
-    var sex = window.document.getElementsByName('sex')
-    console.log(sex)
+    if (nasc.value.length == 0 || Number(nasc.value) > ano){
+        window.alert('[ERRO] Verifique os dados e tente novamente!')
+    } else {
+        var idade = Number(ano) - Number(nasc.value)
+        var sex = window.document.getElementsByName('sex')
+        var genero = ''
+        if (sex[0].checked){
+            genero = 'Masculino'
+        } else {
+            genero = 'Feminino'
+        }
+        
+    }
 }
