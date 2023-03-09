fb91c91bcde219d3bdfb76ae80cdad215bc815cc
P-Lucas-S <68026747+P-Lucas-S@users.noreply.github.com>
2023-03-08 23:28:10 -0300

    fim do curso

--- aula16ex/modelo/script.js
+++ aula16ex/modelo/script.js
@@ -8,15 +8,34 @@ function adicionar() {
         var item = document.createElement('option')
         item.text = `Valor ${num.value} adicionado.`
         lis.appendChild(item)
+        num.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista')
     }
+    num.value = ''
+    num.focus()
 }
 function finalizar() {
-    tab.sort()
     var final = document.getElementById('final')
-    var item = document.createElement('p')
-    final.appendChild(item)
-    item.text = `Ao todo temos ${tab.length} números cadastrados`
-    item.text = `O maior valor informado foi ${tab[tab.length-1]}`
+    // Qantidade de números cadastrados
+    let quant = tab.length
+    let maior = tab[0]
+    let menor = tab[0]
+    let soma = 0
+    for(let pos in tab){
+        soma += Number(tab[pos])
+        if (tab[pos] > maior){
+            maior = tab[pos]
+        }
+        if (tab[pos] < menor){
+            menor = tab[pos]
+        }
+    }
+    let media = soma / quant
+    final.innerHTML = `<p>Ao todo, temos ${quant} números cadastrados</p>`
+    final.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+    final.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+    final.innerHTML += `<p>Somando todos os valores temos ${soma}</p>`
+    final.innerHTML += `<p>A média dos valores digitados é ${media}</p>`
+
 }
